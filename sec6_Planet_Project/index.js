20afe43aeebde1e42f52e0b7802fc29db28f3923
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-08-10 09:45:58 +0530

    SECTION_DONE

--- sec6_Planet_Project/index.js
+++ sec6_Planet_Project/index.js
@@ -1,16 +1,37 @@
-const {parse} = require('csv-parse');
-const fs = require('fs');
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
 const results = [];
+const habitablePlanets = [];
 
-fs.createReadStream('kepler_data.csv')
-  .on('data', (data) => {
-    results.push(data);
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
-  .on('err',(err)=>{
+  .on("err", (err) => {
     console.log(err);
   })
-  .on('end',()=>{
-    console.log(results);
-    console.log('done');
+  .on("end", () => {
+    console.log(habitablePlanets.map((planet)=>{
+      return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+    console.log("done");
   });
